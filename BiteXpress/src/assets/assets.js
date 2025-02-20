69d30101b796974f0ca98bd4586a1ec658135574
RahulNaik15 <rahulnaik039.com>
2025-02-21 00:33:55 +0530

    added new files

---
+++ BiteXpress/src/assets/assets.js
@@ -0,0 +1,43 @@
+import menu_1 from './menu_1.avif'
+import menu_2 from './menu_2.avif'
+import menu_3 from './menu_3.avif'
+import menu_4 from './menu_4.avif'
+import menu_5 from './menu_5.avif'
+import menu_6 from './menu_6.avif'
+import menu_7 from './menu_7.avif'
+import menu_8 from './menu_8.avif'
+
+export const menu_list = [
+    {
+        menu_name: "Salad",
+        menu_image: menu_1
+    },
+    {
+        menu_name: "Rolls",
+        menu_image: menu_2
+    },
+    {
+        menu_name: "Biryani",
+        menu_image: menu_3
+    },
+    {
+        menu_name: "Burgers",
+        menu_image: menu_4
+    },
+    {
+        menu_name: "Cake",
+        menu_image: menu_5
+    },
+    {
+        menu_name: "Pure Veg",
+        menu_image: menu_6
+    },
+    {
+        menu_name: "Pasta",
+        menu_image: menu_7
+    },
+    {
+        menu_name: "Noodles",
+        menu_image: menu_8
+    },
+]
--- BiteXpress/src/components/Navbar/Navbar.jsx
+++ BiteXpress/src/components/Navbar/Navbar.jsx
@@ -4,22 +4,30 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import { faBasketShopping } from "@fortawesome/free-solid-svg-icons";
 import logo from "../../assets/logo.png";
+import { useState } from 'react';
 
 const Navbar = () => {
+
+    const [menu, setMenu] = useState("");
+    
+
   return (
       <>
       <div className='navbar'>
               <img src={logo} alt="logo" style={{ width: "18vw", height: "9vh" }} ></img>
               <ul className='my-ul'>
-                  <li>Home</li>
-                  <li>Menu</li>
-                  <li>Mobile-App</li>
-                  <li>Contact Us</li>
+                  <li onClick={()=>setMenu("home")} className={menu==="home"?"active":""}>Home</li>
+                  <li onClick={()=>setMenu("menu")} className={menu==="menu"?"active":""}>Menu</li>
+                  <li onClick={()=>setMenu("mobile-app")} className={menu==="mobile-app"?"active":""}>Mobile-App</li>
+                  <li onClick={()=>setMenu("contact-us")} className={menu==="contact-us"?"active":""}>Contact Us</li>
               </ul>  
               
               <div className='nav-right'>
-                  <FontAwesomeIcon icon={faMagnifyingGlass} style={{ fontSize: "25px", color:"#49557e" }} /> 
-                  <FontAwesomeIcon icon={faBasketShopping} style={{ fontSize: "25px", color:"#49557e" }} />
+                  <FontAwesomeIcon icon={faMagnifyingGlass} style={{ fontSize: "25px", color: "#49557e" }} /> 
+                  <div className='navbar-basket'>
+                      <FontAwesomeIcon icon={faBasketShopping} style={{ fontSize: "25px", color: "#49557e" }} />
+                      <div className='dot'></div>
+                  </div>
                   <button>Sign In</button>
               </div>
               
